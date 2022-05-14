2e46f17271978f59fef90a1ee146ac9a0296c7cb
licorace <13918539500@163.com>
2022-05-15 00:23:25 +0800

    test(just test): test

--- src/components/nav-menu/src/nav-menu.vue
+++ src/components/nav-menu/src/nav-menu.vue
@@ -2,7 +2,7 @@
   <div class="nav-menu">
     <div class="logo">
       <img class="img" src="~@/assets/img/logo.svg" alt="logo" />
-      <span class="title" v-if="!collapse">Vue3+TS</span>
+      <span class="title" v-if="!collapse">Vue3+TS+CMS</span>
     </div>
     <el-menu
       :default-active="defaultValue"
